7e4dbe542789a49c8227ac003d86a2f734ef4ba3
jack <jagaachieve@gmail.com>
2020-09-26 12:48:37 +0530

    Demo session

--- 01_indentation.py
+++ 01_indentation.py
@@ -1,6 +1,26 @@
-a=1
-print(a)
-x=False
-if x:
-    print('false')
-print('true')
+# a=1
+# print(a)
+# x=False
+# if x:
+#     print('false')
+# print('true')
+
+lst1 =[]
+print(lst1)
+
+tup1 =()
+print(tup1)
+
+lst2 =[2]
+print(lst2)
+
+tup2 = (4)
+print(tup2)
+
+dct2={10:20}
+print(dct2)
+
+
+
+
+
